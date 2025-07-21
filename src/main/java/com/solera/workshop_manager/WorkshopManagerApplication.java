f8a5deee98fae1d1a526eaa24fe4087c5e4ad627
Julieta <julieta.vargas@solera.com>
2025-07-21 15:37:52 -0600

    Inicializando el proyecto

---
+++ src/main/java/com/solera/workshop_manager/WorkshopManagerApplication.java
@@ -0,0 +1,13 @@
+package com.solera.workshop_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopManagerApplication.class, args);
+	}
+
+}
